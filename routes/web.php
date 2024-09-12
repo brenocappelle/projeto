2889af8bea9a63f888a01c3dcfe8878175cca05e
Breno Cappelle <brenocappellealmeida@gmail.com>
2024-09-12 14:05:52 -0300

    resource

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UsuarioController;
 
-Route::get('/', [UsuarioController::class, 'index'] )->name('usuario.index'); 
 
-Route::get('/usuario/{id?}', [UsuarioController::class, 'show'] )->name('usuario.show');
+
 
 
